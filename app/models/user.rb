e9bd2b9af466cb933f03efbeff0060854c8a9455
Stoyama <Stoyama@MacBook.local>
2021-03-29 16:45:37 +0900

    add_username_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :username, uniqueness: true
 
   def update_without_current_password(params, *options)
     if params[:password].blank? && params[:password_confirmation].blank?
