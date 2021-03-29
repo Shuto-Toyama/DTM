fe59fc92f58702dd7fb5f391878d41c7fdcaac1e
Stoyama <Stoyama@MacBook.local>
2021-03-29 15:57:00 +0900

    add_comment_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :post, dependent: :destroy
   has_many :likes
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
