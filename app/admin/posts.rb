443dfdc1e380585e72c54c6535bfbe521e384a20
Stoyama <Stoyama@MacBook.local>
2021-04-04 05:40:23 +0900

    add_posts/footer.link

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,17 +1,16 @@
-ActiveAdmin.register User do
-  permit_params :name, :username, :email
-
+ActiveAdmin.register Post do
+  permit_params :text
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :profile_photo, :username
+  # permit_params :text, :user_id
   #
   # or
   #
   # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :profile_photo, :username]
+  #   permitted = [:text, :user_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
