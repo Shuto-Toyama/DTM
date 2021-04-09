e21dbc213f2a00d5977b4bd7121b1e36f601c53d
Stoyama <Stoyama@MacBook.local>
2021-04-09 14:26:57 +0900

    add_formatter

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :url, presence: true
+  before_save do
+    format_url = YoutubeUrlFormatter.format(url)
+    if format_url.present?
+      self.url = format_url
+    else
+      self.errors.add(:url, "YouTubeのURL以外は無効です")
+    end
+  end
 end
