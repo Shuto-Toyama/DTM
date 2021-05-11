9c912a5f5f6285e9e30e2c51f2d190ef45d616f0
Stoyama <Stoyama@MacBook.local>
2021-05-11 10:52:07 +0900

    admin_userのお問い合わせ権限追加

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,17 +1,17 @@
-ActiveAdmin.register Photo do
+ActiveAdmin.register Contact do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :image, :post_id
+  permit_params :contact_name, :contact_email, :content, :remote_ip
   #
   # or
   #
   # permit_params do
-  #   permitted = [:image, :post_id]
+  #   permitted = [:contact_name, :contact_email, :content, :remote_ip]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-
+  
 end
--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Post do
-  permit_params :text
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :text, :user_id
+  permit_params :text, :user_id
   #
   # or
   #
