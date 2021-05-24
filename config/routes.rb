c4c6cb53dc819bb453316a67a80fa37b40239e8f
Stoyama <Stoyama@MacBook.local>
2021-05-24 12:29:43 +0900

    ゲストログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
                        omniauth_callbacks: "users/omniauth_callbacks",
                        registrations: "users/registrations",
+                       passwords: "users/passwords",
                      }
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   root "posts#index"
   get "/users/:id", to: "users#show", as: "user"
   resources :users, only: %i(show destroy)
